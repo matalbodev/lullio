50b6230ac1a6df6bd2ee2f9fa7c54eb3bceb3701
matalbodev <mathieualbore@gmail.com>
2025-04-10 08:50:16 +0200

    feat: minimal setup on

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }}>
+    <StatusBar style="auto" />
+  </Stack>;
 }
